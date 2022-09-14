aa5beb154a02c03cf40d29f6bf1e86e8717e1516
dibyaranjan.das <dibyaranjan.das@grabpay.com>
2022-09-14 19:17:37 +0530

    logging for server starting point is fixed

--- server/server.go
+++ server/server.go
@@ -13,6 +13,7 @@ import (
 func StartServer() {
 	r := mux.NewRouter()
 	router.InitializeRouter(r)
-	log.Fatal(http.ListenAndServe(":4000", r))
+	logger.GetMyLogger().Info("Routers are initialised")
 	logger.GetMyLogger().Warn("Server started successfully")
+	log.Fatal(http.ListenAndServe(":4000", r))
 }
